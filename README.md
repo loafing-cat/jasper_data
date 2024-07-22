17ef075ffceaf5294a5ae6046ce3375b73c30de2
loafing-cat <nathannguyen801@gmail.com>
2024-07-22 16:05:14 -0700

    updated data

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This repository tracks Jasper's data over time to monitor his FIP treatment progress.
 
 ![image info](jasper3.jpeg)
-![image info](jasper_mean_weight_violin_weekly_time_series_2024_07_16.png)
-![image info](jasper_weight_daily_time_series_2024_07_16.png)
+![image info](jasper_mean_weight_violin_weekly_time_series_2024_07_22.png)
+![image info](jasper_weight_daily_time_series_2024_07_22.png)
 ![image info](biomarkers_graph_2024_07_12.png)
 ![A cat playing with a ball of yarn](https://media.giphy.com/media/GeimqsH0TLDt4tScGw/giphy-downsized.gif)
--- scripts/jasper_weekly_daily_weight_plots_secure_script.R
+++ scripts/jasper_weekly_daily_weight_plots_secure_script.R
@@ -193,7 +193,7 @@ ggsave(
   filename = paste0(here("images", "line", "jasper_weight_daily_time_series"), format(Sys.Date(), "_%Y_%m_%d"), ".png"),
   plot = p2,
   dpi = 600,
-  width = 14,
+  width = 16,
   height = 8
 )
 
@@ -201,7 +201,7 @@ ggsave(
 npg_palette_alpha_0.7 <- c("#E64B35B2", "#4DBBD5B2", "#00A087B2", "#3C5488B2", "#F39B7FB2", "#8491B4B2", "#91D1C2B2", "#DC0000B2", "#7E6148B2")
 npg_palette_alpha_1 <-  c("#E64B35FF", "#4DBBD5FF", "#00A087FF", "#3C5488FF", "#F39B7FFF", "#8491B4FF", "#91D1C2FF", "#DC0000FF", "#7E6148FF")
 
-custom_colors <- c("#F39B7FFF", "#4DBBD5FF", "#91D1C2FF", "#7fc8ff", "#8491B4B2","#d3860e", "#ffd145", "#A9C1A8", "#E0218A", "#ffbe79","#ff8c87" )
+custom_colors <- c("#F39B7FFF", "#4DBBD5FF", "#91D1C2FF", "#7fc8ff", "#8491B4B2","#d3860e", "#ffd145", "#A9C1A8", "#E0218A", "#ffbe79","#ff8c87","#91D1C2FF")
 
 
 ###
@@ -256,6 +256,7 @@ p3 <- daily_weight %>%
   scale_fill_manual(values = custom_colors) +
   scale_y_continuous(expand = c(0, 0), n.breaks = 8) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = length(unique(complete_weeks$week)))) +
+  # scale_x_continuous(breaks = 1:12) +
   # theme_minimal() +
   theme_bw() +
   # theme_clean() +
